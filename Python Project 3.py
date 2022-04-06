99ebc41a26ec89d9f26c4ec311754570bb93d273
Ajaybcss <99288228+Ajaybcss@users.noreply.github.com>
2022-04-06 14:21:31 -0400

    Add files via upload

---
+++ Python Project 3.py
@@ -0,0 +1,14 @@
+import cmath
+
+a = float(input('\nWhat is a? -'))
+b = float(input('\nWhat is b? -'))
+c = float(input('\nWhat is c? -'))
+
+d = (b**2) - (4*a*c)
+
+solution1 = (-b-cmath.sqrt(d))/(2*a)
+solution2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('\nThe solution are {0} & {1}'.format(solution1,solution2))
+
+
